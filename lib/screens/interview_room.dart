2bb48107f662b3a35915ce3d05274dd34b041197
Zainab <zainabsajid1218@gmail.com>
2026-04-24 23:00:41 -0700

    Add InterviewRoom widget with placeholder

--- lib/screens/interview_room.dart
+++ lib/screens/interview_room.dart
@@ -1,1 +1,15 @@
+import 'package:flutter/material.dart';
 
+class InterviewRoom extends StatefulWidget {
+  const InterviewRoom({super.key});
+
+  @override
+  State<InterviewRoom> createState() => _InterviewRoomState();
+}
+
+class _InterviewRoomState extends State<InterviewRoom> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
